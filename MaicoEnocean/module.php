38e75fce15738cb0e7bf2cbfe7db2911931e58d4
norbert-schmerold <74622166+norbert-schmerold@users.noreply.github.com>
2023-01-20 22:46:00 +0100

    Free Device ID

--- MaicoEnocean/module.php
+++ MaicoEnocean/module.php
@@ -11,4 +11,26 @@ class MaicoEnocean extends IPSModule {
     public function MeineErsteEigeneFunktion() {
         echo $this->InstanceID;
     }
+
+
+    #================================================================================================
+	protected function FreeDeviceID()  //Ermitteln der nächsten freien Adresse des Gateways
+	#================================================================================================
+	{
+		$Gateway = @IPS_GetInstance($this->InstanceID)["ConnectionID"];
+		if($Gateway == 0) return;
+		$Devices = IPS_GetInstanceListByModuleType(3);             # alle Geräte
+		$DeviceArray = array();
+		foreach ($Devices as $Device){
+			if(IPS_GetInstance($Device)["ConnectionID"] == $Gateway){
+				$config = json_decode(IPS_GetConfiguration($Device));
+				if(!property_exists($config, 'DeviceID'))continue;
+				if(is_integer($config->DeviceID)) $DeviceArray[] = $config->DeviceID;
+			}
+		}
+	
+		for($ID = 1; $ID<=256; $ID++)if(!in_array($ID,$DeviceArray))break;
+		return $ID == 256?0:$ID;
+	}
+		
 }
