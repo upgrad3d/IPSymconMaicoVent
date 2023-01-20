83894ead8c811c7f20157cf476b628980f0781cb
norbert-schmerold <74622166+norbert-schmerold@users.noreply.github.com>
2023-01-20 22:37:32 +0100

    module.php angelegt

---
+++ MaicoEnocean/module.php
@@ -0,0 +1,14 @@
+<?php
+
+class MaicoEnocean extends IPSModule {
+    /**
+    * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+    * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+    *
+    * ABC_MeineErsteEigeneFunktion($id);
+    *
+    */
+    public function MeineErsteEigeneFunktion() {
+        echo $this->InstanceID;
+    }
+}
